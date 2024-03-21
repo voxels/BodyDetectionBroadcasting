de41f9c530012fc1ecc485fe8c9f7bd26dbe6a75
voxels <voxels@gmail.com>
2024-03-21 09:06:54 -0400

    broadcasting

--- BodyDetectionBroadcasting/ContentView.swift
+++ BodyDetectionBroadcasting/ContentView.swift
@@ -16,6 +16,9 @@ struct ContentView : View {
     @StateObject private var shareplayModel = ContentViewShareplayModel(useShareplay: true)
     @State private var useShareplay = true
     @State private var useJournal = false
+    @State private var playerModel = PlayerModel()
+    private let videoURLString = "https://customer-02f18dl7ud0edmeq.cloudflarestream.com/eb980b10c52b699e5b544aceb9b610d9/manifest/video.m3u8"
+    private let audioURLString = "https://streams.radiomast.io/295116c5-c102-44a8-b4ed-667dbfb3c4a7"
     var body: some View {
         if useShareplay {
             if !shareplayModel.isReady {
@@ -30,54 +33,72 @@ struct ContentView : View {
                         }
                     }
             } else {
-                ARViewContainer(model: model, shareplayModel: shareplayModel, useShareplay: useShareplay)
-                    .onChange(of: shareplayModel.encodedJointData, { oldValue, newValue in
-                        if useJournal {
-                            guard let newValue = newValue, let journal = shareplayModel.journal, let groupSession = shareplayModel.groupSession, groupSession.activeParticipants.count > 1 else {
-                                print("Active participants: \(shareplayModel.groupSession?.activeParticipants)")
-                                return
-                            }
-                            Task {
-                                do {
-                                    let data = try JSONEncoder().encode(newValue)
-                                    
-                                    let attachment = try await journal.add(data)
-                                    print("Sending attachment:\(attachment.id)")
-                                    shareplayModel.attachmentHistory.append(attachment)
-                                } catch {
-                                    print(error)
+                ZStack(alignment: .bottomTrailing, content: {
+                    ARViewContainer(model: model, shareplayModel: shareplayModel, useShareplay: useShareplay)
+                        .onChange(of: shareplayModel.encodedJointData, { oldValue, newValue in
+                            if useJournal {
+                                guard let newValue = newValue, let journal = shareplayModel.journal, let groupSession = shareplayModel.groupSession, groupSession.activeParticipants.count > 1 else {
+                                    print("Active participants: \(shareplayModel.groupSession?.activeParticipants)")
+                                    return
                                 }
-                            }
-                        } else {
-                            guard let newValue = newValue, let messenger = shareplayModel.messenger, let groupSession = shareplayModel.groupSession, groupSession.activeParticipants.count > 1 else {
-                                print("Active participants: \(shareplayModel.groupSession?.activeParticipants)")
-                                return
-                            }
-                            for key in newValue.keys {
-                                let jointData = newValue[key]
                                 Task {
                                     do {
-                                        let data = try JSONEncoder().encode(jointData)
-                                        try await messenger.send(data)
+                                        let data = try JSONEncoder().encode(newValue)
+                                        
+                                        let attachment = try await journal.add(data)
+                                        print("Sending attachment:\(attachment.id)")
+                                        shareplayModel.attachmentHistory.append(attachment)
                                     } catch {
                                         print(error)
                                     }
                                 }
+                            } else {
+                                guard let newValue = newValue, let messenger = shareplayModel.messenger, let groupSession = shareplayModel.groupSession, groupSession.activeParticipants.count > 1 else {
+                                    print("Active participants: \(shareplayModel.groupSession?.activeParticipants)")
+                                    return
+                                }
+                                for key in newValue.keys {
+                                    let jointData = newValue[key]
+                                    Task {
+                                        do {
+                                            let data = try JSONEncoder().encode(jointData)
+                                            try await messenger.send(data)
+                                        } catch {
+                                            print(error)
+                                        }
+                                    }
+                                }
+                            }
+                        })
+                        .onAppear(perform: {
+                            shareplayModel.startAdvertisingDevice()
+                        })
+                        .edgesIgnoringSafeArea(.all)
+                        .onDisappear(perform: {
+                            shareplayModel.stopAdvertisingDevice()
+                        })
+                        .onChange(of: shareplayModel.groupSession?.state) { oldValue, newValue in
+                            if case .invalidated = newValue {
+                                shareplayModel.isReady = false
                             }
                         }
-                    })
-                    .onAppear(perform: {
-                        shareplayModel.startAdvertisingDevice()
-                    })
-                    .edgesIgnoringSafeArea(.all)
-                    .onDisappear(perform: {
-                        shareplayModel.stopAdvertisingDevice()
-                    })
-                    .onChange(of: shareplayModel.groupSession?.state) { oldValue, newValue in
-                        if case .invalidated = newValue {
-                            shareplayModel.isReady = false
-                        }
-                    }
+                    PlayerViewController(model: $playerModel)
+                        .frame(width:320, height:240)
+                        .padding(32)
+                        .onDisappear(perform: {
+                            playerModel.stop()
+                        })
+                        .task { @MainActor in
+                            do {
+                                playerModel.loadAudio(urlString: audioURLString)
+                                try await playerModel.loadVideo(URL(string:videoURLString)!,presentation: .fullWindow )
+                                playerModel.play()
+                            } catch {
+                                print(error)
+                            }
+                        }.foregroundStyle(.clear)
+
+                })
             }
         } else {
                 ARViewContainer(model: model, shareplayModel: shareplayModel, useShareplay: useShareplay)
---
+++ BodyDetectionBroadcasting/PlayerViewController.swift
@@ -0,0 +1,36 @@
+//
+//  PlayerViewController.swift
+//  BodyDetectionBroadcasting
+//
+//  Created by Michael A Edgcumbe on 3/15/24.
+//
+
+
+import AVKit
+import SwiftUI
+
+// This view is a SwiftUI wrapper over `AVPlayerViewController`.
+struct PlayerViewController: UIViewControllerRepresentable {
+    
+    @Binding public var model:PlayerModel
+
+    func makeUIViewController(context: Context) -> AVPlayerViewController {
+        
+        let controller = model.makePlayerViewController()
+        controller.videoGravity = .resizeAspect
+        return controller
+        
+    }
+    
+    func updateUIViewController(_ uiViewController: AVPlayerViewController, context: Context) {
+        
+    }
+    
+    static func dismantleUIViewController(
+        _ uiViewController: Self.UIViewControllerType,
+        coordinator: Self.Coordinator
+    ){
+        uiViewController.player?.replaceCurrentItem(with: nil)
+        print("Dismantled")
+    }
+}
